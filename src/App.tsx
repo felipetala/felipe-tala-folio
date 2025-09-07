35c56c35c8859d1252058f12d21965b6cf143da6
Felipe Tala <felipetala@example.com>
2025-09-07 11:44:54 -0700

    Fix blank page issue: Add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/felipe-tala-folio">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/resume" element={<Resume />} />
